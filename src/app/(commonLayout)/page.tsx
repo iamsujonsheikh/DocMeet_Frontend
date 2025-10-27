787d6ccdc5ee7c7809a2a10bf5b6d518fe78ac66
sujoncoder <sujonsheikh.in@gmail.com>
2025-10-27 10:18:11 +0600

    done basic setup

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen gap-2 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Hello next lavel dev</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+}
